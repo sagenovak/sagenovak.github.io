df6e33a254193b8b185d4ad2d0cd9014b00c2b52
sagenovak <sagenovak23@gmail.com>
2025-08-28 22:42:19 +0000

    add ASD projects

---
+++ fsd-projects/platformer/platformer.js
@@ -0,0 +1,55 @@
+$(function () {
+  // initialize canvas and context when able to
+  canvas = document.getElementById("canvas");
+  ctx = canvas.getContext("2d");
+  window.addEventListener("load", loadJson);
+
+  function setup() {
+    if (firstTimeSetup) {
+      halleImage = document.getElementById("player");
+      projectileImage = document.getElementById("projectile");
+      cannonImage = document.getElementById("cannon");
+      $(document).on("keydown", handleKeyDown);
+      $(document).on("keyup", handleKeyUp);
+      firstTimeSetup = false;
+      //start game
+      setInterval(main, 1000 / frameRate);
+    }
+
+    // Create walls - do not delete or modify this code
+    createPlatform(-50, -50, canvas.width + 100, 50); // top wall
+    createPlatform(-50, canvas.height - 10, canvas.width + 100, 200, "navy"); // bottom wall
+    createPlatform(-50, -50, 50, canvas.height + 500); // left wall
+    createPlatform(canvas.width, -50, 50, canvas.height + 100); // right wall
+
+    //////////////////////////////////
+    // ONLY CHANGE BELOW THIS POINT //
+    //////////////////////////////////
+
+    // TODO 1 - Enable the Grid
+    // toggleGrid();
+
+    // TODO 2 - Create Platforms
+    createPlatform(200, 600, 100, 20, "#fed35d");
+    createPlatform(400, 500, 200, 20, "#fed35d");
+    createPlatform(800, 400, 100, 20, "#fed35d");
+    createPlatform(900, 275, 100, 20, "#fed35d");
+    createPlatform(1300, 400, 50, 20, "#fed35d");
+
+    // TODO 3 - Create Collectables
+    createCollectable("kitty", 600, 200);
+    createCollectable("yarn", 1200, 450);
+    createCollectable("kitty", 100, 400);
+
+    // TODO 4 - Create Cannons
+    createCannon("bottom", 650, 1500);
+    createCannon("right", 350, 2000);
+    createCannon("right", 725, 1500);
+
+    //////////////////////////////////
+    // ONLY CHANGE ABOVE THIS POINT //
+    //////////////////////////////////
+  }
+
+  registerSetup(setup);
+});
